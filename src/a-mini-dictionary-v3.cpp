684ed28e842e0a2390c8c5d093ef30c5cb2d1bd3
Tu Thanh <tuthanh10825@gmail.com>
2024-07-19 08:02:18 +0700

    Add project files.

---
+++ src/a-mini-dictionary-v3.cpp
@@ -0,0 +1,12 @@
+﻿// a-mini-dictionary-v3.cpp : Defines the entry point for the application.
+//
+
+#include "a-mini-dictionary-v3.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
