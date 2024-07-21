4fe2cb750a2c68d6f91473ed38cda1f2d1b9af78
Thuanle27 <thuanle.qt@gmail.com>
2024-07-21 17:06:43 +0700

    fix bug: Remove wrong stream

--- source/main.cpp
+++ source/main.cpp
@@ -1,28 +1,17 @@
 ﻿// a-mini-dictionary-v3.cpp : Defines the entry point for the application.
-<<<<<<< Updated upstream
-#include <wx/wx.h>
-#include "frame.h"
-=======
+
 #include "MainFrame.h"
 #include "HomeFrame.h"
->>>>>>> Stashed changes
 class myApp : public wxApp
 {
 	virtual bool OnInit()
 	{
-<<<<<<< Updated upstream
-		mainFrame* MainFrame = new mainFrame(wxT("dictionary"));
-		MainFrame->SetClientSize(800, 800);
-		MainFrame->Show(true); 
-=======
-		//mainFrame* MainFrame = new mainFrame(wxT("dictionary"));
-		//MainFrame->SetClientSize(WIDTH, HEIGHT);
-		//MainFrame->Show(true); 
+
 
 		HomeFrame* homeFrame = new HomeFrame("Dictonary");
 		homeFrame->Show();
 
->>>>>>> Stashed changes
+
 		return true; 
 	}
 };
